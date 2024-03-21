44a9254a1d1a0f153f173f5944a29a661316d7d7
Paul Tsogbe <paul.tsogbe@gmail.com>
2024-03-21 17:16:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const formidableMiddleware = require("express-formidable")
+const app = express()
+app.use(formidableMiddleware())
+
+//  import data json
+const covid = require("./data/covid-19-api.json")
+
+
+// Statistiques globales(pour tous les pays)
+
+app.get("/", (req, res) =>{
+    // on crée un tableau vide (variable)
+    let results = []
+    for(let i = 0; i < covid.length; i++){
+        let country = {
+            name: covid[i].name,
+            cases: covid[i].latest_data.confirmed,//nombre de personne atteind
+            deaths: covid[i].latest_data.deaths,// ...     ..         décédées
+            recovered: covid[i].latest_data.recovered//..     ...       guérir
+        }
+       // à chaque tour de boucle, on crée un objet "country" dans lequel on ajoute 4 paires clé/valeur, et on l'ajoute dans le tableau "results"
+        results.push(country)
+    }
+
+
+    res.json(results)
+
+
+})
+// Statistiques par pays
+
+app.post("/by-country", (req, res) =>{
+   try {
+    const country = req.fields.country;
+    //  on stocke dans la variable "country" le nom du pays récupéré dans les paramètres query
+    const results = [];
+    for (let i = 0; i < country.length; i++) {
+       if (country === covid[i].name) {
+         // cette condition permet de trouver, dans les données globales, les informations concernant le pays recherché
+         let country = {
+            name: covid[i].name,
+            cases: covid[i].latest_data.confirmed,//nombre de personne atteind
+            deaths: covid[i].latest_data.deaths,// ...     ..         décédées
+            recovered: covid[i].latest_data.recovered//..     ...       guérir
+         };
+         results.push(country);
+
+       }
+        
+    }
+
+    res.json(results);
+   } catch (error) {
+    res.status(404).res.json({message : "it is not fund"})
+   }
+})
+
+app.listen(3000 , () =>{
+    console.log("Server Started")
+})
